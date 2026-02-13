2c28a86bf7c45ac7b89826d927bdabdb31062a9b
seboo <25958061+seboo@users.noreply.github.com>
2026-02-13 14:23:46 +0100

    LUT-31926 : Demand API : add demands without notifications

--- src/java/fr/paris/lutece/plugins/notificationstore/business/DemandDAO.java
+++ src/java/fr/paris/lutece/plugins/notificationstore/business/DemandDAO.java
@@ -102,8 +102,8 @@ public final class DemandDAO implements IDemandDAO
             + " FROM notificationstore_demand WHERE reference = ?";
 
     private static final String SQL_QUERY_IDS_BY_CUSTOMER_ID_AND_DEMANDTYPE_ID = "SELECT distinct(gd.uid) " + " FROM notificationstore_demand gd "
-            + " INNER JOIN notificationstore_notification gn ON ( gd.id = gn.demand_id and gd.demand_type_id = gn.demand_type_id and gd.customer_id = gn.customer_id ) "
-            + " INNER JOIN notificationstore_notification_content gc ON ( gn.id = gc.notification_id ) " + " WHERE gd.customer_id = ? ";
+            + " LEFT JOIN notificationstore_notification gn ON ( gd.id = gn.demand_id and gd.demand_type_id = gn.demand_type_id and gd.customer_id = gn.customer_id ) "
+            + " LEFT JOIN notificationstore_notification_content gc ON ( gn.id = gc.notification_id ) " + " WHERE gd.customer_id = ? ";
 
     private static final String SQL_QUERY_IDS_BY_STATUS = "SELECT distinct gd.uid " + " FROM notificationstore_demand gd "
             + " JOIN notificationstore_notification gn ON ( gd.id = gn.demand_id and gd.demand_type_id=gn.demand_type_id and gd.customer_id=gn.customer_id) "
